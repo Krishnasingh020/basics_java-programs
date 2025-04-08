91af41250634a286c7fd8ce50a9f76082cb984bb
Krishna singh <140987227+Krishnasingh020@users.noreply.github.com>
2025-04-08 07:51:47 +0530

    Add files via upload
    A program to change binary to decimal .

--- Javabasics.java
+++ Javabasics.java
@@ -1,30 +1,21 @@
 import java.util.Scanner;
 public class Javabasics{
-    // Function to check if a number is prime
-    public static boolean isprime(int n){
-        for (int i=2;i<n-1;i++){
-            if (n%i==0){
-            
-                return false; // Return 0 if not prime
-            }
+    public static void dectobin(int binum){
+        int pow =0;
+        int decnum = 0;
+        int mynum = binum;
+        while(binum >0){
+            int lastdigit =binum%10;
+            decnum = decnum + (lastdigit * (int)Math.pow(2,pow));
+            pow++;  
+            binum = binum/10;
         }
-        return true; // Return 1 if prime
+        System.out.println("Decimal of " + mynum + " is " + decnum);
     }
-    // Function to print all prime numbers in a given range
-    public static void primesinrange(int n){
-        for (int i=2;i<n;i++){
-            if (isprime(i)){
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-    }
-    //main function 
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int n = sc.nextInt();
-        primesinrange(n);// Call the function to print prime numbers in the range
-        
-    }
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a binary number: ");
+    int binum = sc.nextInt();
+    dectobin(binum);
 }
+}    
